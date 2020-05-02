495e0f8f2aa0dd6f9bac8db2e35df8d5ea6ceea0
bhupendra <bhupendrapt95@gmail.com>
2020-05-02 10:49:52 +0530

    fixed Integration and Context Testing

--- src/test/java/io/bhupendra/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/io/bhupendra/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,28 +1,39 @@
 package io.bhupendra.repositories;
 
+import io.bhupendra.bootstrap.RecipeBootstrap;
 import io.bhupendra.domain.UnitOfMeasure;
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Disabled
-@ExtendWith(MockitoExtension.class)
+@DataMongoTest
 public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
+    @Autowired
+    CategoryRepository categoryRepository;
+    @Autowired
+    RecipeRepository recipeRepository;
 
     @BeforeEach
     public void setUp() throws Exception {
+        recipeRepository.deleteAll();
+        categoryRepository.deleteAll();
+        unitOfMeasureRepository.deleteAll();
 
+        RecipeBootstrap recipeBootstrap =
+                new RecipeBootstrap(categoryRepository, recipeRepository, unitOfMeasureRepository);
+
+        recipeBootstrap.onApplicationEvent(null);
     }
 
     @Test
@@ -36,7 +47,6 @@ public class UnitOfMeasureRepositoryIT {
     public void findByDescriptionCup() {
         Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Cup");
 
-//        assertNull(unitOfMeasure);
         assertEquals("Cup",
                 unitOfMeasure.get().getDescription());
     }
